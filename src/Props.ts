208378861a955ffc649244a4b387631a7dd3e756
Aleksandr Tihonov <a.tihonov@polymatica.com>
2021-06-09 20:13:03 +0300

    demo

--- src/Props.ts
+++
@@ -1,9 +0,0 @@
-export type Task = {
-  id: string;
-  task: string;
-  date: string;
-  closed: boolean;
-  priority: number;
-  comments: string[];
-  finished: boolean;
-};
--- src/components/Tasks/styles.ts
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from "@material-ui/core/styles";
-
-export const useStyles = makeStyles({
-  tasks: {
-    marginTop: 100,
-    overflow: "auto",
-    maxHeight: 550,
-  },
-});
